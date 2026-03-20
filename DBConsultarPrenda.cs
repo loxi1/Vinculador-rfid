3cc1d7da33236103cdca80c90955bfc4ecb6be56
loxi1 <anibal.cayetano@gmail.com>
2026-03-20 12:52:30 -0500

    change, cambios

--- DBConsultarPrenda.cs
+++ DBConsultarPrenda.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace DS9908R_App
 {
---
+++ VinculacionRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS9908R_App
+{
+    public class VinculacionRequest
+    {
+        public string CodigoBarras { get; set; }
+        public string Rfid { get; set; }
+        public string HojaMarcacion { get; set; }
+        public string CodTrabajador { get; set; }
+        public string Empresa { get; set; }
+    }
+}
